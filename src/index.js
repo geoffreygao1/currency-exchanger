f2e5739b62d179f48031bf15d650f8d183662242
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-13 12:04:48 +0900

    Added clearError function

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function convertCurrency(currency, value) {
         const errorMessage = `There was a problem accessing the currency conversion data from  ExchangeRate-API for ${currency}: ${currencyResponse['error-type']}`;
         throw new Error(errorMessage);
       }
-      printConversion(currencyResponse, value)
+      printConversion(currencyResponse, value);
     })
     .catch(function (error) {
       printError(error);
@@ -23,9 +23,23 @@ function calculateConversion(exchangeRate, value) {
 }
 
 // UI Logic
+function printConversion(currencyResponse, value) {
+  const outputCurrencyType = document.getElementById("outputCurrencyType").value;
+  let outputValue = document.getElementById("outputValue");
+  let conversionRates = currencyResponse['conversion_rates'];
+  outputValue.value = calculateConversion(conversionRates[outputCurrencyType], value);
+}
 
+function printError(error) {
+  document.getElementById('error').innerText = error;
+}
+
+function clearError() {
+  document.getElementById('error').innerText = null;
+}
 
 function handleFormSubmission() {
+  clearError();
   const inputValue = document.getElementById("inputValue").value;
   const inputCurrencyType = document.getElementById("inputCurrencyType").value;
   convertCurrency(inputCurrencyType, inputValue);
