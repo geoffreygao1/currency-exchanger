4ae967cccb4bbba3c9ab4742eeff66ed7e2ca20c
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-13 11:44:52 +0900

    Added event handler for any change in form

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './css/styles.css';
 import ExchangeRateService from './services/exchange-rate-service.js';
 
 // Business Logic
-function convertCurrency() {
+function convertCurrency(currency, value) {
   ExchangeRateService.getExchangeRate(currency)
     .then(function (currencyResponse) {
       if (currencyResponse instanceof Error) {
         const errorMessage = `There was a problem accessing the currency conversion data from  ExchangeRate-API for ${currency}: ${currencyResponse['error-type']}`;
         throw new Error(errorMessage);
       }
-      printConversion(currencyResponse, currency)
+      printConversion(currencyResponse, currency, value)
     })
     .catch(function (error) {
       printError(error);
@@ -19,3 +19,13 @@ function convertCurrency() {
 }
 
 // UI Logic
+
+function handleFormSubmission() {
+  const inputValue = document.getElementById("inputValue").value;
+  const inputCurrency = document.getElementById("inputCurrencyType").value;
+  convertCurrency(inputCurrencyType, inputValue);
+}
+
+window.addEventListener("load", function () {
+  document.querySelector('form').addEventListener("change", handleFormSubmission);
+});
