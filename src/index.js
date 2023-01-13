49155abd00326e3744b52a9672c7b21cd179828f
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-13 15:19:07 +0900

    Added flag icon and updateFlags function

--- src/index.js
+++ src/index.js
@@ -28,7 +28,6 @@ function updateConversion(newCurrencyType, value) {
   return (value * conversionRates[newCurrencyType]).toFixed(2);
 }
 
-
 // UI Logic
 function printConversion(currencyResponse, value) {
   const outputCurrencyType = document.getElementById("outputCurrencyType").value;
@@ -45,8 +44,31 @@ function clearError() {
   document.getElementById('error').innerText = null;
 }
 
+function updateFlags() {
+  const currencyToCountryCode = {
+    "USD": "https://cdn-icons-png.flaticon.com/512/197/197484.png",
+    "EUR": "https://cdn-icons-png.flaticon.com/512/197/197615.png",
+    "JPY": "https://cdn-icons-png.flaticon.com/512/197/197604.png",
+    "AUD": "https://cdn-icons-png.flaticon.com/512/197/197507.png",
+    "CAD": "https://cdn-icons-png.flaticon.com/512/197/197430.png",
+    "GBP": "https://cdn-icons-png.flaticon.com/512/197/197374.png"
+  };
+
+  document.getElementById("inputFlag").innerHTML = null;
+  document.getElementById("outputFlag").innerHTML = null;
+  const inputCurrencyType = document.getElementById("inputCurrencyType").value;
+  const outputCurrencyType = document.getElementById("outputCurrencyType").value;
+  let inputFlag = document.createElement("img");
+  inputFlag.setAttribute("src", `${currencyToCountryCode[inputCurrencyType]}`);
+  let outputFlag = document.createElement("img");
+  outputFlag.setAttribute("src", `${currencyToCountryCode[outputCurrencyType]}`);
+  document.getElementById("inputFlag").append(inputFlag);
+  document.getElementById("outputFlag").append(outputFlag);
+}
+
 function handleInputSubmission() {
   clearError();
+  updateFlags();
   const inputValue = document.getElementById("inputValue").value;
   const inputCurrencyType = document.getElementById("inputCurrencyType").value;
   convertCurrency(inputCurrencyType, inputValue);
@@ -54,6 +76,8 @@ function handleInputSubmission() {
 
 function handleOutputSubmission() {
   clearError();
+  updateFlags();
+
   const inputValue = document.getElementById("inputValue").value;
   const outputCurrencyType = document.getElementById("outputCurrencyType").value;
   let outputValue = document.getElementById("outputValue");
@@ -63,4 +87,5 @@ function handleOutputSubmission() {
 window.addEventListener("load", function () {
   document.querySelector('form#inputCurrencyForm').addEventListener("change", handleInputSubmission);
   document.querySelector('form#outputCurrencyForm').addEventListener("change", handleOutputSubmission);
+  updateFlags();
 });
