8097c72a3ba5036e066f101423af209e1c151f76
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-13 11:39:19 +0900

    Added converCurrency function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-//Placeholder script
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRateService from './services/exchange-rate-service.js';
 
+// Business Logic
+function convertCurrency() {
+  ExchangeRateService.getExchangeRate(currency)
+    .then(function (currencyResponse) {
+      if (currencyResponse instanceof Error) {
+        const errorMessage = `There was a problem accessing the currency conversion data from  ExchangeRate-API for ${currency}: ${currencyResponse['error-type']}`;
+        throw new Error(errorMessage);
+      }
+      printConversion(currencyResponse, currency)
+    })
+    .catch(function (error) {
+      printError(error);
+    });
+}
+
+// UI Logic
