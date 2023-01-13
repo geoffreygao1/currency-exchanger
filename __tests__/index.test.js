3530c969434a63243408ad3ba28c25b79ff9cb5b
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-13 16:01:50 +0900

    Removed jest dependencies

--- __tests__/index.test.js
+++
@@ -1,9 +0,0 @@
-// Placeholder test script
-
-// Example:
-// describe('Triangle', () => {
-//   test('should correctly determine whether three lengths make an isosceles triangle', () => {
-//     const isoscTriangle = new Triangle(5,5,7)
-//     expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
-//   });
-// });
